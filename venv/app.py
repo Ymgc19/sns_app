893c4c4a5c74ca0769039428339707033a2b6260
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-12 08:39:00 +0900

    change

--- venv/app.py
+++ venv/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("やまんぬ.R")
-st.caption("東北大学の文学研究科所属のカエルがお送りします")
+st.title("論文共有用")
+st.caption("東北大学の文学研究科所属のカエルがお送りします．")
 st.subheader("自己紹介")
 st.text("このサイトでは，読んだ論文の概要や感想などについて述べていきます．")
+
+title = st.text_input('タイトル')
+
+submit_btn = st.bottun("送信")
+cancel_btn = st.bottun("キャンセル")
+
+print(f'submit_btn: {submit_btn}')
+print(f'cancel_btn: {cancel_btn}')

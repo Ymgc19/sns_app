f7fba746cc5abcd1398c32b18757ffab4e3121cd
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-12 10:38:47 +0900

    調整

--- venv/app.py
+++ venv/app.py
@@ -15,9 +15,10 @@ st.text("東北大学　文学研究科　計算人文社会学　山口侑平")
 col1, col2 = st.columns(2)
 
 # 表にまとめるときの形式
-data = pd.DataFrame(columns=["読んだ日", "タイトル", "著者名", "キーワード", "分野", "概要", "手法", "評価"])
+
 
 with col1:
+    data = pd.DataFrame(columns=["読んだ日", "タイトル", "著者名", "キーワード", "分野", "概要", "手法", "評価"])
     st.subheader("論文情報の追加")
 
     with st.form(key = "論文情報の入力"):
@@ -39,14 +40,7 @@ with col1:
         if submit_btn:
             st.text("論文情報を登録しました")
             data = data.append({"読んだ日": date, "タイトル": title, "著者名": author, "キーワード": keywords, "分野": field, "概要": summary, "手法": method, "評価": recommend}, ignore_index=True)
-            title = ""
-            date = ""
-            author = ""
-            keywords = ""
-            field = ""
-            summary = ""
-            method = ""
-            recommend = 0
+
 
 
 with col2:

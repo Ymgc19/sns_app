ce2495fbf5a0c79b12e7c35de68c5a09856fcf2c
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-12 10:23:31 +0900

    調整

--- venv/app.py
+++ venv/app.py
@@ -31,7 +31,7 @@ with col1:
         cancel_btn = st.form_submit_button("キャンセル")
         if submit_btn:
             st.text("論文情報を登録しました")
-            data = data.append({"読んだ日": date, "タイトル": title, "著者名": author, "キーワード": keywords, "分野": field, "概要": summary, "手法": method, "評価": recommend}, ignore_index=True)
+            data = data.append({"読んだ日": date, "タイトル": title, "著者名": author, "キーワード": keywords, "分野": field, "概要": summary, "手法": method, "評価": recommend}, ignore_index=False)
 
 with col2:
     st.table(data)

0438448b39547553ff86bc634bdbce3376ea0df7
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-12 10:12:29 +0900

    調整

--- venv/app.py
+++ venv/app.py
@@ -8,7 +8,7 @@ st.text("東北大学　文学研究科　計算人文社会学　山口侑平")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("ここで論文の情報を入力をします")
+    st.subheader("論文情報の追加")
     data = pd.DataFrame(columns=["読んだ日", "タイトル", "著者名", "キーワード", "分野", "概要", "手法", "評価"])
 
     with st.form(key = "論文情報の入力"):

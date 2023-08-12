d75958192d215ad750107cc291f7870993ac928b
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-12 10:46:51 +0900

    調整

--- venv/app.py
+++ venv/app.py
@@ -12,11 +12,14 @@ st.set_page_config(
 st.title("論文共有")
 st.text("東北大学　文学研究科　計算人文社会学　山口侑平")
 
+
+#表を作成するためのデータフレーム
+data = pd.DataFrame(columns=["読んだ日", "タイトル", "著者名", "キーワード", "分野", "概要", "手法", "評価"])
+
 col1, col2 = st.columns(2)
 
 
 with col1:
-    data = pd.DataFrame(columns=["読んだ日", "タイトル", "著者名", "キーワード", "分野", "概要", "手法", "評価"])
     st.subheader("論文情報の追加")
 
     with st.form(key = "論文情報の入力"):
@@ -35,12 +38,12 @@ with col1:
     # ボタン
         submit_btn = st.form_submit_button("送信")
         cancel_btn = st.form_submit_button("キャンセル")
-    if submit_btn:
-        st.text("論文情報を登録しました")
-        data = data.append({"読んだ日": date, "タイトル": title, "著者名": author, "キーワード": keywords,
-                            "分野": field, "概要": summary, "手法": method, "評価": recommend},
-                            ignore_index=True)
-
+        if submit_btn:
+            st.text("論文情報を登録しました")
+            data = data.append({"読んだ日": date, "タイトル": title, "著者名": author, "キーワード": keywords,
+                                "分野": field, "概要": summary, "手法": method, "評価": recommend},
+                                ignore_index=True)
+            data = data
 
 
 with col2:

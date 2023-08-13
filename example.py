2696062f045f8b98b05a66bf91bbfc313691c28c
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-13 18:03:20 +0900

    変更

--- example.py
+++ example.py
@@ -1,5 +1,9 @@
 import os
 
 path = __file__
-path = os.path.splitext(os.path.basename(path))[0]
+path = str(os.path.splitext(os.path.basename(path))[0])
+
+ext = ".csv"
+
+path = path + ext
 print(path)

13368347a72a248c5c1edd9e8ec5f8da0f0728fe
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-08-15 16:12:22 +0900

    試み

--- venv/HOME.py
+++ venv/HOME.py
@@ -4,7 +4,7 @@ import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
 # plt.use('Agg')  # バックエンドをAggに設定
-# import japanize_matplotlib
+import japanize_matplotlib
 
 st.set_page_config(
     page_title="Full Screen App",
